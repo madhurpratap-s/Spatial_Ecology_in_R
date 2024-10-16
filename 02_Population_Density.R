345ff4e4fc6eb94e965483acf3a8468608eeffa0
madhurpratap-s <madhurpratapsingh2002@gmail.com>
2024-10-16 21:29:53 +0200

    Update 02_Population_Density.R for 16-10-2024

--- 02_Population_Density.R
+++ 02_Population_Density.R
@@ -28,3 +28,47 @@ densitymap
 
 plot(densitymap)
 points(bei, col="green")
+
+# Plotting the maps one beside the other
+par(mfrow=c(1,2))
+plot(elevation2)
+plot(densitymap)
+
+# Exercise: make a multiframe with maps one on top of the other
+par(mfrow=c(2,1))
+plot(elevation2)
+plot(densitymap)
+
+# one frined to clear graphs
+dev.off()
+plot(elevation2)
+
+# Changin colors to maps
+cl <- colorRampPalette(c("red", "orange", "yellow"))(3)
+plot(densitymap, col=cl)
+
+cl <- colorRampPalette(c("red", "orange", "yellow"))(10)
+plot(densitymap, col=cl)
+
+cl <- colorRampPalette(c("red", "orange", "yellow"))(100)
+plot(densitymap, col=cl)
+
+# search your browser for "colors in R" 
+# http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
+
+# Exercise: change the color ramp palette using different colors
+
+cln <- colorRampPalette(c("purple1", "orchid2", "palegreen3", "paleturquoise"))(100)
+plot(densitymap, col=cln)
+
+# Exrcise: build a multiframe and plot the densitymap with two different color ramp palettes one beside the other
+
+par(mfrow=c(1,2))
+
+cln <- colorRampPalette(c("purple1", "orchid2", "palegreen3", "paleturquoise"))(100)
+plot(densitymap, col=cln)
+
+clg <- colorRampPalette(c("green4", "green3", "green2", "green1", "green"))(100)
+plot(densitymap, col=clg)
+
+dev.off()
