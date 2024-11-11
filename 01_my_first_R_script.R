ab097e6690824fb7161fd74a854fd1f1e68e0ce9
madhurpratap-s <madhurpratapsingh2002@gmail.com>
2024-11-11 11:48:37 +0100

    Update comments in 01_my_first_R_script.R

--- 01_my_first_R_script.R
+++ 01_my_first_R_script.R
@@ -1,46 +1,55 @@
 # This script is created after the Population_Density.R file as I joined the course on 10-10-2024
 
-# There are five important concepts:
-# 1. objects
-# 2. the assignment (<-)
-# 3. comments (like this one!)
-# 4. functions
-# 5. arrays
+# Here I can write anything I want!
+# First, we look at some basic R operations and plotting examples
 
-2 + 3 # this is my first operation in R
+# We can use R as a calculator
+2 + 3 # This is my first operation in R (Simple Addition)
+
+# There are five important concepts that we should know:
+# 1. Objects
+# 2. The assignment (<-)
+# 3. Comments (like this one!)
+# 4. Functions
+# 5. Arrays
 
 # It is important to make use of objects
-cato <- 2 + 3  # the result of the operation is assigned to an object called cato
-cato
+madhur <- 2 + 3  # The result of the operation is assigned to an object called madhur
+madhur # Display the value of 'madhur'
 
-maria <- 4 + 2
-maria
+maria <- 4 + 2 # Assign another addition result to an object
+maria # Display the value of 'maria'
 
-cato + maria
+madhur + maria # This displays the sum of the values assigned to 'madhur' and 'maria'
 
-cato ^ maria
+madhur ^ maria # This displays the outcome of raising the value assigned to 'madhur' to the value assigned to 'maria'
 
-(cato + maria) ^ cato
+(madhur + maria) ^ cato # Another example of an operation we can perform
 
-# arrays
+# Creating arrays (vectors) for plotting
 andrea <- c(10, 15, 20, 50, 70) # in a function arguments are separated by commas  
-andrea
+andrea # Display 'andrea' array
 
-sofia <- c(100, 200, 300, 400, 500) # an array is a set of different elemnts
-sofia
+sofia <- c(100, 200, 300, 400, 500) # another array
+sofia # Display 'sofia' array
 
+# Make a scatter plot with 'sofia' on the x-axis and 'andrea' on the y-axis
 plot(sofia, andrea)
 
-plot(sofia, andrea, pch=19)
+# pch parameter in plot function specifies the plotting character we can use
+plot(sofia, andrea, pch=19) # Plot with solid circles as plotting symbol
+# The link given below has a list of the most commonly used pch values in R and additional plotting tips:
 # https://www.datanovia.com/en/blog/pch-in-r-best-tips/
 
-plot(sofia, andrea, pch=19, cex=2)
+# cex paramter in plot function is used to control the size of the elements in the plot
+plot(sofia, andrea, pch=19, cex=2) # Plot with points 2 times the default size
+plot(sofia, andrea, pch=19, cex=0.5) # Plot with points 0.5 times the default size
+plot(sofia, andrea, pch=19, cex=.5) # We can write .5 instead of 0.5 without any problem
 
-plot(sofia, andrea, pch=19, cex=0.5)
+# col parameter in the plot function is used to set the colour of the elements in the plot
+plot(sofia, andrea, pch=19, cex=2, col="blue") # Example with blue colored elements
+# The link below displays more than 100 colors that can be called by a name in R:
+# https://r-graph-gallery.com/42-colors-names.html
 
-plot(sofia, andrea, pch=19, cex=.5)
-
-plot(sofia, andrea, pch=19, cex=2, col="blue")
-# colors in R: https://r-graph-gallery.com/42-colors-names.html
-
+# xlab and ylab parameters are used to label the x-axis and y-axis of the plot respectively
 plot(sofia, andrea, pch=19, cex=2, col="blue", xlab="CO2", ylab="amount of fruits")
