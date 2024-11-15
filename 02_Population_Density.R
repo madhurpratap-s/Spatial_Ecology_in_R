ccca8a9d476ed4c83a0a04acc82c923d97ca122f
madhurpratap-s <madhurpratapsingh2002@gmail.com>
2024-11-15 22:15:28 +0100

    Update comments in 02_Population_Density.R

--- 02_Population_Density.R
+++ 02_Population_Density.R
@@ -1,74 +1,101 @@
-# How to calculate the density of individuals in a population
+# Population Ecology analysis using Spatstat package
 
-# Installing the spatstat package
+# spatstat is an R package used for spatial statistics with a strong focus on analysing spatial point patterns in 2d,
+# (with some support for 3D and very basic support for space-time)
+
+# Install and load the required package for point pattern analysis
 install.packages("spatstat")
-
-# Recalling the package
 library(spatstat)
 
-# dataset
+# We'll use the 'bei' dataset included in the spatstat package
+# It contains the locations of trees in a geographical region
+# More details at https://cran.r-project.org/web/packages/spatstat/index.html
 bei
 
+# Plot the tree locations
 plot(bei)
+
+# Plot the tree locations with a solid circle (pch = 19)
 plot(bei, pch=19)
+
+# Modify the plot by changing symbol size (cex)
 plot(bei, pch=19, cex=.5)
 
+# Additional dataset: 'bei.extra' which includes elevation and gradient data
 bei.extra
+
+# Plot 'elev' (Elevation) and 'grad' (Gradient) images present in bei.extra
 plot(bei.extra)
 
-# Extracting data
+# In order to take only the elevation part of the dataset, let's use $ sign to link
+# Extract and plot the elevation data from 'bei.extra'
 elevation <- bei.extra$elev
 plot(elevation)
 
-elevation <- bei.extra[[1]]
+# There is an alternative method to extract elements from a list
+# We use double brackets here as we want to extract a single element from a list
+elevation2 <- bei.extra[[1]]
+# For tables or subsets, we use single brackets
 
-# Density map starting from points
+# We can create a kernel density estimate of the spatial point pattern
+# This density estimate smooths the point pattern into a continous surface, providing an estimate of density of points across the study region
 densitymap <- density(bei)
-densitymap
-
 plot(densitymap)
-points(bei, col="green")
+# We can overlay the original tree points on the density map 
+points(bei, cex = 0.1)
 
-# Plotting the maps one beside the other
-par(mfrow=c(1,2))
+######## DAY 2
+
+# A multiframe in R is a collection of multiple plots displayed together
+
+# Plotting the elevation and density maps one beside the other
+par(mfrow=c(1,2)) # This means we have 1 row and 2 columns
 plot(elevation2)
 plot(densitymap)
 
 # Exercise: make a multiframe with maps one on top of the other
+# Solution: use mfrow=c(2,1) so that we have 2 rows and 1 column
 par(mfrow=c(2,1))
 plot(elevation2)
 plot(densitymap)
 
-# one frined to clear graphs
+# One friend to close the current graphics device, clearing the last plot
 dev.off()
-plot(elevation2)
 
-# Changin colors to maps
+# colorRampPalette creates custom color palettes by interpolating between specified colours
+
+# Create a custom color palette with 3 colors and plot density map using cl
 cl <- colorRampPalette(c("red", "orange", "yellow"))(3)
 plot(densitymap, col=cl)
 
+# Increase the number of colors in the pallete to see improvement in smoothness of plot
 cl <- colorRampPalette(c("red", "orange", "yellow"))(10)
 plot(densitymap, col=cl)
 
+# Further increase smoothness by going to a 100 colors
 cl <- colorRampPalette(c("red", "orange", "yellow"))(100)
 plot(densitymap, col=cl)
 
-# search your browser for "colors in R" 
-# http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
+# To find all available colours, one can search in browser for "colors in R" 
+# R colors cheat-sheet by Dr. Ying Wei: http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
 
 # Exercise: change the color ramp palette using different colors
+# Solution:
 
-cln <- colorRampPalette(c("purple1", "orchid2", "palegreen3", "paleturquoise"))(100)
+cln <- colorRampPalette(c("gold", "darkorchid", "mediumaquamarine", "salmon"))(100)
 plot(densitymap, col=cln)
 
 # Exrcise: build a multiframe and plot the densitymap with two different color ramp palettes one beside the other
+# Solution:
 
 par(mfrow=c(1,2))
 
-cln <- colorRampPalette(c("purple1", "orchid2", "palegreen3", "paleturquoise"))(100)
+cln <- colorRampPalette(c("turquoise", "coral", "plum", "forestgreen"))(100)
 plot(densitymap, col=cln)
 
-clg <- colorRampPalette(c("green4", "green3", "green2", "green1", "green"))(100)
+clg <- colorRampPalette(c("crimson", "saddlebrown", "slateblue", "goldenrod"))(100)
 plot(densitymap, col=clg)
 
-dev.off()
+# Color customization: To make maps color-blind friendly, it's important to avoid using certain color combinations
+# People with color vision deficiencies (e.g., Daltonism) may have difficulty distinguishing between red, green, and blue
+# To ensure inclusivity, consider using color palettes that are distinguishable to everyone
